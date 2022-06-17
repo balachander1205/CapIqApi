5fc99ab86363afb79612361347cc383e5f25e73b
balachander1205 <balachander1205@gmail.com>
2022-06-17 09:06:02 +0530

    Initial Commit

---
+++ src/main/java/com/api/capIq/CapIqApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.capIq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapIqApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapIqApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/capIq/CapIqApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.capIq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapIqApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
